072fc0400de725fd0f9fe16c34f4ba0170ddfee7
King Wang <king.wang@perficient.com>
2014-04-14 14:49:51 +0800

    Add Controller for return JSON。 used to ajax call...

---
+++ src/main/java/com/wdxxl/exam/controller/JsonController.java
@@ -0,0 +1,22 @@
+package com.wdxxl.exam.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class JsonController {
+	
+	@RequestMapping(value="/json", method=RequestMethod.GET)
+	public @ResponseBody Object  getJson(){
+		Map<String,Object> result = new HashMap<String,Object>();
+		result.put("name", "King");
+		result.put("age", "12");
+		return result;
+	}
+	
+}
